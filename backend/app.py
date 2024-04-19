f283194f1ffbd0f7cccefb3625ed8a99c1c1301c
Yuki <yuki_yamakawa@kokuyo.com>
2024-04-19 17:49:03 +0900

    DBの更新

--- backend/app.py
+++ backend/app.py
@@ -11,16 +11,32 @@ CORS(app)
 def index():
     return "<p>Flask top page!</p>"
 
-@app.route("/mapping", methods=['GET'])
-def read_all_users():
-    model = [
-        dbmodels.User,
-        dbmodels.Occupation,
-        dbmodels.User_grid,
-        dbmodels.Island_grid,
-        ]
-    result = crud.select_for_mapping(model)
-    return result, 200
-
 # @app.route('/login', methods=['POST'])
 # def login():
+
+@app.route("/mapping", methods=['GET'])
+def mapping():
+    model = [
+        dbmodels.User,
+        dbmodels.Login_status,
+        dbmodels.Occupation_attributes,
+        dbmodels.Occupation_attributes_image,
+        dbmodels.User_grid,
+        dbmodels.Island_grid
+        ]
+    result = crud.select_mapping(model)
+    return result, 200
+
+@app.route("/user_profile", methods=['GET'])
+def show_user_profile():
+    model = [
+        dbmodels.User,
+        dbmodels.Details,
+        dbmodels.Sex,
+        dbmodels.Living_place,
+        dbmodels.Birth_place,
+        dbmodels.Spend_time,
+        dbmodels.By_when
+        ]
+    result = crud.select_profile(model)
+    return result, 200
--- backend/db_control/dbmodels.py
+++ backend/db_control/dbmodels.py
@@ -6,29 +6,57 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Base(DeclarativeBase):  # sqlalchemyのORM。DeclarativeBaseを継承し、Baseクラスの定義。
     pass
 
+
 class User(Base):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_name: Mapped[str] = mapped_column()
-    age: Mapped[int] = mapped_column()
-    career: Mapped[str] = mapped_column()
-    sex_id: Mapped[int] = mapped_column()  # FK
-    address_id: Mapped[int] = mapped_column()  # FK
-    occupation_id: Mapped[int] = mapped_column(ForeignKey("occupation.id"))
+    login_info_id: Mapped[int] = mapped_column(ForeignKey("login_info.id"))
+    login_status_id: Mapped[int] = mapped_column(ForeignKey("login_status.id"))
+    occupation_attributes_id: Mapped[int] = mapped_column(ForeignKey("occupation_attributes.id"))
     grid_id: Mapped[int] = mapped_column(ForeignKey("user_grid.id"))
-    login_info_id: Mapped[int] = mapped_column()  # FK
-    details_id: Mapped[int] = mapped_column()  # FK
+    details_id: Mapped[int] = mapped_column(ForeignKey("details.id"))
 
-    # occupation: Mapped['Occupation'] = relationship(back_populates='user')
-    # grid: Mapped['User_grid'] = relationship(back_populates='user')
+    # login_info: Mapped['Login_info'] = relationship(back_populates='user')
+    # login_status: Mapped['Login_status'] = relationship(back_populates='user')
+    # occupation_attributes: Mapped['Occupation_attributes'] = relationship(back_populates='user')
+    # user_grid: Mapped['User_grid'] = relationship(back_populates='user')
 
-class Occupation(Base):
-    __tablename__ = 'occupation'
+
+class Login_info(Base):
+    __tablename__ = 'login_info'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    login_id: Mapped[str] = mapped_column()
+    login_pw: Mapped[str] = mapped_column()
+
+    user: Mapped['User'] = relationship(backref='login_info')
+
+
+class Login_status(Base):
+    __tablename__ = 'login_status'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    login_status: Mapped[str] = mapped_column()
+    status_color: Mapped[str] = mapped_column()
+
+    user: Mapped['User'] = relationship(backref='login_status')
+
+
+class Occupation_attributes(Base):
+    __tablename__ = 'occupation_attributes'
     id: Mapped[int] = mapped_column(primary_key=True)
-    occupation_name: Mapped[str] = mapped_column()
-    occupation_attributes_id: Mapped[str] = mapped_column()  # FK
+    occupation_attributes_name: Mapped[str] = mapped_column()
+    occupation_attributes_image_id: Mapped[int] = mapped_column(ForeignKey("occupation_attributes_image.id"))
 
-    user: Mapped['User'] = relationship(backref='occupation')
+    user: Mapped['User'] = relationship(backref='occupation_attributes')
+    # occupation_attributes_image: Mapped['Occupation_attributes_image'] = relationship(back_populates='occupation_attributes')
+
+class Occupation_attributes_image(Base):
+    __tablename__ = 'occupation_attributes_image'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    occupation_attributes_image: Mapped[str] = mapped_column()
+
+    occupation_attributes: Mapped['Occupation_attributes'] = relationship(backref='occupation_attributes_image')
+
 
 class User_grid(Base):
     __tablename__ = 'user_grid'
@@ -37,7 +65,7 @@ class User_grid(Base):
     user_y_grid: Mapped[float] = mapped_column()
     island_grid_id: Mapped[int] = mapped_column(ForeignKey("island_grid.id"))
 
-    user: Mapped['User'] = relationship(backref='grid')
+    user: Mapped['User'] = relationship(backref='user_grid')
     # island_grid: Mapped['Island_grid'] = relationship(back_populates='user_grid')
 
 class Island_grid(Base):
@@ -50,23 +78,71 @@ class Island_grid(Base):
     user_grid: Mapped['User_grid'] = relationship(backref='island_grid')
 
 
+class Details(Base):
+    __tablename__ = 'details'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    sex_id: Mapped[int] = mapped_column(ForeignKey("sex.id"))
+    age: Mapped[int] = mapped_column()
+    living_place_id: Mapped[int] = mapped_column(ForeignKey("living_place.id"))
+    birth_place_id: Mapped[int] = mapped_column(ForeignKey("birth_place.id"))
+    industry: Mapped[str] = mapped_column()
+    occupation: Mapped[str] = mapped_column()
+    certification: Mapped[str] = mapped_column()
+    skill: Mapped[str] = mapped_column()
+    career: Mapped[str] = mapped_column()
+    self_introduction: Mapped[str] = mapped_column(String(100))
+    want: Mapped[str] = mapped_column(String(30))
+    spend_time_id: Mapped[int] = mapped_column(ForeignKey("spend_time.id"))
+    by_when_id: Mapped[int] = mapped_column(ForeignKey("by_when.id"))
+
+    # sex: Mapped['Sex'] = relationship(back_populates='details')
+    # living_place: Mapped['Place'] = relationship(back_populates='details')
+    # birth_place: Mapped['Place'] = relationship(back_populates='details')
+    # spend_time: Mapped['Spend_time'] = relationship(back_populates='details')
+    # by_when: Mapped['By_when'] = relationship(back_populates='details')
+
+class Sex(Base):
+    __tablename__ = 'sex'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sex_name: Mapped[str] = mapped_column()
+
+    details: Mapped['Details'] = relationship(backref='sex')
+
+class Living_place(Base):
+    __tablename__ = 'living_place'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    living_place_name: Mapped[str] = mapped_column()
+
+    details: Mapped['Details'] = relationship(backref='living_place')
+
+
+class Birth_place(Base):
+    __tablename__ = 'birth_place'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    birth_place_name: Mapped[str] = mapped_column()
+
+    details: Mapped['Details'] = relationship(backref='birth_place')
+
+
+class Spend_time(Base):
+    __tablename__ = 'spend_time'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    spend_time: Mapped[str] = mapped_column()
+
+    details: Mapped['Details'] = relationship(backref='spend_time')
+
+class By_when(Base):
+    __tablename__ = 'by_when'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    by_when: Mapped[str] = mapped_column()
+
+    details: Mapped['Details'] = relationship(backref='by_when')
+
+
 
 # 以下、補足行
 
-
-# --Mapped[Optional[str]]について詳しく説明します：
-# Mapped: SQLAlchemyで、このフィールドがデータベーステーブルのカラムにマッピングされていることを示します。
-# Mapped型は、カラムの値をPythonの属性として扱うことができるようにするものです。
-
-# Optional[str]: この部分はPythonの型ヒントで、str型か、もしくはNoneが許容されることを意味します。
-# Optional[str]は、基本的にUnion[str, None]と同じです。
-# これは、該当するデータベースのカラムがNULL値を許容する場合に使用されます。
-
-# このコンテキストでのMapped[Optional[str]]は、データベースのカラムが文字列型で、値が設定されていない場合にNULLを許容することを意味しています。
-# つまり、このフィールドには文字列が入るか、もしくはNULL（PythonではNone）が入ることがあります。
-
-# 例えば、nickname属性にMapped[Optional[str]]が使用されている場合、
-# ユーザーにニックネームが設定されていないことを許容しており、
-# その場合、データベースではその値がNULLとして保存されます。
-
+# Mapped[Optional[str]]は、データベースのカラムが文字列型で、値が設定されていない場合にNULLを許容することを意味しています。
 # https://chat.openai.com/share/bdd731c5-5cf8-41d0-babf-24b1b5ae236f
+
+# self_introduction: Mapped[str] = mapped_column(String(100))は、100文字以内のstring型
